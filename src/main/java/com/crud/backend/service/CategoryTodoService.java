b3fedee129bc02747b533ce7022024f54082c578
jpabloallin <jpac.647@gmail.com>
2022-05-05 14:21:02 -0500

    Creation of CategoryTodoService class

---
+++ src/main/java/com/crud/backend/service/CategoryTodoService.java
@@ -0,0 +1,41 @@
+package com.crud.backend.service;
+
+import com.crud.backend.dao.CategoryTodoDaoImplement;
+import com.crud.backend.entity.Category;
+import com.crud.backend.entity.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryTodoService {
+
+    @Autowired
+    private CategoryTodoDaoImplement categoryTodoService;
+
+    public List<Category> findAllCategories(){
+        return categoryTodoService.findAllCategories();
+    }
+
+    public Category createCategory(Category category){
+        return categoryTodoService.createCategory(category);
+    }
+
+    public Category createTodo(Todo todo){
+        return categoryTodoService.createTodo(todo);
+    }
+
+    public Category updateTodo(Todo todo){
+        return categoryTodoService.updateTodo(todo);
+    }
+
+    public void deleteCategory(Category category){
+        categoryTodoService.deleteCategory(category);
+    }
+
+    public void deleteTodo(Todo todo){
+        categoryTodoService.deleteTodo(todo);
+    }
+
+}
