15293054db930ddc617808a9640cb0980d51f5f4
jpabloallin <jpac.647@gmail.com>
2022-05-05 14:15:05 -0500

    Creation Category entity

---
+++ src/main/java/com/crud/backend/entity/Category.java
@@ -0,0 +1,54 @@
+package com.crud.backend.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Category")
+@Table(name = "category")
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Todo> todos = new ArrayList<>();
+
+    public Category addTodo(Todo todo) {
+        this.todos.add(todo);
+
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<Todo> todos) {
+        this.todos = todos;
+    }
+}
